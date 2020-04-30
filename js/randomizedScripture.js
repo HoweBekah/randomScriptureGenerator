faa3ecce73d6d90f250285216ecdac895a5b7cd0
Bekah Howe <46459346+HoweBekah@users.noreply.github.com>
2020-04-30 02:09:29 -0700

    all the things work currently still need to debug

--- js/randomizedScripture.js
+++ js/randomizedScripture.js
@@ -8,24 +8,9 @@ request.open("GET", requestURL);
 request.responseType = "text";
 request.send();
 
-// request.onload = function () {
-//   const scriptureText = request.responseText;
-//   //console.log(scriptureText);
-//   const scriptureData = JSON.parse(scriptureText);
-//   console.log(scriptureData);
-//   randomBom(scriptureData);
-//   // randomOldT(scriptureData);
-//   // randomNewT(scriptureData);
-//   // randomDC(scriptureData);
-//   // randomGP(scriptureData);
-//   // randomAll(scriptureData);
-// };
-
 function randomBom() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
   var scripts = scriptureData.works[0];
   var ranBook = Math.floor(Math.random() * (scripts.bom.length - 1));
   var bookLength = scripts.bom[ranBook].chapters.length - 1;
@@ -35,70 +20,57 @@ function randomBom() {
   if (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent = "Read " + scripts.bom[ranBook].chapters[ranChap].reference;
+  var test = document.createElement("h3");
+  test.textContent = scripts.bom[ranBook].chapters[ranChap].reference;
   section.appendChild(test);
 }
 
 function randomOldT() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
   var scripts = scriptureData.works[3];
   var ranBook = Math.floor(Math.random() * (scripts.ot.length - 1));
   var bookLength = scripts.ot[ranBook].chapters.length - 1;
   var ranChap = Math.floor(Math.random() * bookLength);
 
-  console.log(bookLength);
   if (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent = "Read " + scripts.ot[ranBook].chapters[ranChap].reference;
+  var test = document.createElement("h3");
+  test.textContent = scripts.ot[ranBook].chapters[ranChap].reference;
   section.appendChild(test);
 }
 function randomNewT() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
   var scripts = scriptureData.works[2];
   var ranBook = Math.floor(Math.random() * (scripts.nt.length - 1));
   var bookLength = scripts.nt[ranBook].chapters.length - 1;
   var ranChap = Math.floor(Math.random() * bookLength);
 
-  console.log(bookLength);
   if (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent = "Read " + scripts.nt[ranBook].chapters[ranChap].reference;
+  var test = document.createElement("h3");
+  test.textContent = scripts.nt[ranBook].chapters[ranChap].reference;
   section.appendChild(test);
 }
 function randomDC() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
   var scripts = scriptureData.works[1];
   var ranBook = Math.floor(Math.random() * (scripts.sections.length - 1));
-  // var bookLength = scripts.ot[ranBook].chapters.length - 1;
-  // var ranChap = Math.floor(Math.random() * bookLength);
 
-  //console.log(bookLength);
   if (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent = "Read " + scripts.sections[ranBook].reference;
+  var test = document.createElement("h3");
+  test.textContent = scripts.sections[ranBook].reference;
   section.appendChild(test);
 }
 function randomGP() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
   var scripts = scriptureData.works[4];
   var ranBook = Math.floor(Math.random() * (scripts.pogp.length - 1));
   var bookLength = scripts.pogp[ranBook].chapters.length - 1;
@@ -108,24 +80,40 @@ function randomGP() {
   if (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent =
-    "Read " + scripts.pogp[ranBook].chapters[ranChap].reference;
+  var test = document.createElement("h3");
+  test.textContent = scripts.pogp[ranBook].chapters[ranChap].reference;
   section.appendChild(test);
 }
 function randomAll() {
   var scriptureText = request.responseText;
-  //console.log(scriptureText);
   var scriptureData = JSON.parse(scriptureText);
-  console.log(scriptureData);
+  var titleDiv = document.getElementById("titlebook");
   var ranBook = Math.floor(Math.random() * 5);
   var scripts = scriptureData.works[ranBook];
-
-  // console.log(bookLength);
-  if (section.firstChild) {
+  if (section.firstChild || titleDiv.firstChild) {
     section.removeChild(section.firstChild);
+    titleDiv.removeChild(titleDiv.firstChild);
   }
-  var test = document.createElement("h2");
-  test.textContent = "Read " + scripts.title;
-  section.appendChild(test);
+  var titleDiv = document.getElementById("titlebook");
+  var bookTitle = document.createElement("h2");
+  bookTitle.textContent = scripts.title;
+  titleDiv.appendChild(bookTitle);
+
+  switch (ranBook) {
+    case 0:
+      return randomBom();
+      break;
+    case 1:
+      return randomDC();
+      break;
+    case 2:
+      return randomNewT();
+      break;
+    case 3:
+      return randomOldT();
+      break;
+    case 4:
+      return randomGP();
+      break;
+  }
 }
